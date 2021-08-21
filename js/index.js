1f84b5456084dac7548b68917239b6719f3e1c64
KaisarH007 <kaisarh007@gmail.com>
2021-08-21 20:03:32 +0600

    modify

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Button Event Handler
 const memory8Gb = document.getElementById('memory-8gb');
 const memory16Gb = document.getElementById('memory-16gb');
 const storage256Gb = document.getElementById('storage-256gb');
@@ -5,16 +6,16 @@ const storage512Gb = document.getElementById('storage-512gb');
 const storage1Tb = document.getElementById('storage-1tb');
 const freeDeliver = document.getElementById('free-delivery');
 const deliveryCharge = document.getElementById('delivery-charge');
-
+//Price field event handler
 let extraMemoryField = document.getElementById('extra-memory-cost');
 let extraStorageField = document.getElementById('extra-storage-cost');
 let extraDeliveryField = document.getElementById('extra-delivery-cost');
-
 const bestPriceField = document.getElementById('best-price');
+//total price event handler
 const totalPrice = document.getElementById('total-price');
 
 
-//Total Price add
+//Total Price add function
 function updateTotalPrice() {
     const extraMemoryCost = Number(extraMemoryField.innerText);
     const extraStorageCost = Number(extraStorageField.innerText);
@@ -24,7 +25,7 @@ function updateTotalPrice() {
     promoPrice.innerText = totalPrice.innerText;
 }
 
-
+//Memory option
 memory8Gb.addEventListener('click', function () {
     extraMemoryField.innerText = '0';
     updateTotalPrice()
@@ -35,6 +36,7 @@ memory16Gb.addEventListener('click', function () {
     updateTotalPrice()
 })
 
+//Storage Option
 storage256Gb.addEventListener('click', function () {
     extraStorageField.innerText = '0';
     updateTotalPrice()
@@ -48,6 +50,7 @@ storage1Tb.addEventListener('click', function () {
     updateTotalPrice()
 })
 
+//Delivery Option
 freeDeliver.addEventListener('click', (function () {
     extraDeliveryField.innerText = '0';
     updateTotalPrice()
@@ -59,17 +62,18 @@ deliveryCharge.addEventListener('click', function () {
 })
 
 
-//Promo price
-const promoField = document.getElementById('promo-field');
+//Promo price 
+let promoField = document.getElementById('promo-field');
 let promoPrice = document.getElementById('promo-price');
 const promoButton = document.getElementById('promo-button');
 
 promoButton.addEventListener('click', function () {
     if (promoField.value == "stevekaku") {
-        promoDiscountPrice = Number(totalPrice.innerText) * 0.75;
+        promoDiscountPrice = Number(totalPrice.innerText) * 0.8; //promo discount price 20% off.
         promoPrice.innerText = promoDiscountPrice;
 
     }
-
+    //clear promo input field
+    promoField.value = '';
 
 })
